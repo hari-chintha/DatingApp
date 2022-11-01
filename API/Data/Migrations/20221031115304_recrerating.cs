d9d4f4684b6e011bb765f59b8727053e45654015
Hari Krishna Chintha <chinthaharikrishna@gmail.com>
2022-10-31 21:41:16 -0500

    End of section 4 - Most changes are related API Net core

---
+++ API/Data/Migrations/20221031115304_recrerating.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class recrerating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
